7d9539b502b13c8a160cfa6c207885647042ce7f
AdmiN <AdmiN@AdmiN-PC>
2020-07-20 01:41:42 +0530

    new changes

--- spring-boot-hello-world-example/src/main/java/com/boraji/tutorial/springboot/MainApp.java
+++ spring-boot-hello-world-example/src/main/java/com/boraji/tutorial/springboot/MainApp.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MainApp {
+	//lol
    public static void main(String[] args) {
       System.out.println("Hello World Welcome To Spring-Boot");
    }
